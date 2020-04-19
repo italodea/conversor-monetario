22a905a119242a32fef033ff283f3418403636cd
Ítalo de azevedo Albuquerque <italodeazevedo99@gmail.com>
2020-04-19 00:03:31 -0300

    Finalização do design e funcionalidade com 4 moedas

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final defaultController = TextEditingController();
   final realController = TextEditingController();
   final euroController = TextEditingController();
@@ -38,13 +37,61 @@ class _HomeState extends State<Home> {
   double libra;
 
   String dropdownValue = 'BRL';
+	void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+		bitcController.text = "";
+  }
+  void _realToAll(String text) {
+		if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
 
-  void _realToAll(String text){
     double real = double.parse(text);
-    dolarController.text = "US\$"+((real/dolar).toStringAsFixed(2));
-    bitcController.text = "BTC\$"+(real/bitcoin).toStringAsFixed(2);
-    euroController.text = "€\$"+(real/euro).toStringAsFixed(2);
-    realController.text = "\$"+(real/1).toStringAsFixed(2);
+    dolarController.text = ((real / dolar).toStringAsFixed(2));
+    bitcController.text = (real / bitcoin).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+
+  }
+  void _dolarToAll(String text) {
+		if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double dollar = double.parse(text);
+    realController.text = (dollar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dollar * this.dolar/euro).toStringAsFixed(2);
+    bitcController.text = (dollar * this.dolar/bitcoin).toStringAsFixed(3);
+
+  }
+
+  void _euroToAll(String text) {
+		if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro/dolar).toStringAsFixed(2);
+    bitcController.text = (euro * this.euro/bitcoin).toStringAsFixed(3);
+
+  }
+
+  void _bitcToAll(String text) {
+		if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double bitcoin = double.parse(text);
+    realController.text = (bitcoin * this.bitcoin).toStringAsFixed(2);
+    euroController.text = (bitcoin * this.bitcoin/euro).toStringAsFixed(2);
+    dolarController.text = (bitcoin * this.bitcoin/dolar).toStringAsFixed(2);
+		
   }
 
   @override
@@ -89,11 +136,14 @@ class _HomeState extends State<Home> {
                                 TextStyle(color: Colors.black, fontSize: 25.0),
                             textAlign: TextAlign.center));
                   } else {
-                    dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                    dolar =
+                        snapshot.data["results"]["currencies"]["USD"]["buy"];
                     euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
-                    bitcoin = snapshot.data["results"]["currencies"]["BTC"]["buy"];
+                    bitcoin =
+                        snapshot.data["results"]["currencies"]["BTC"]["buy"];
                     peso = snapshot.data["results"]["currencies"]["ARS"]["buy"];
-                    libra = snapshot.data["results"]["currencies"]["GBP"]["buy"];
+                    libra =
+                        snapshot.data["results"]["currencies"]["GBP"]["buy"];
 
                     return SingleChildScrollView(
                       padding: EdgeInsets.all(20.0),
@@ -116,107 +166,10 @@ class _HomeState extends State<Home> {
                               ),
                             ],
                           ),
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(color: Colors.grey, blurRadius: 2.0)
-                                ]),
-                            child: Row(
-                              children: <Widget>[
-                                Container(
-                                    child: DropdownButton<String>(
-                                  value: dropdownValue,
-                                  icon: Icon(Icons.arrow_drop_down),
-                                  iconSize: 24,
-                                  elevation: 16,
-                                  style: TextStyle(
-                                      fontSize: 20.0, color: Colors.brown),
-                                  underline: Container(
-                                    height: 0,
-                                  ),
-                                  onChanged: (String newValue) {
-                                    setState(() {
-                                      dropdownValue = newValue;
-                                      if (newValue == "BRL"){
-                                      }
-                                    });
-                                  },
-                                  items: <String>["BRL", 'USD', 'EUR', 'BTC']
-                                      .map<DropdownMenuItem<String>>(
-                                          (String value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value),
-                                    );
-                                  }).toList(),
-                                )),
-                                Expanded(
-                                  child: Container(
-                                    child: TextField(
-                                      controller: defaultController,
-                                      keyboardType: TextInputType.number,
-                                      onChanged: _realToAll,
-                                      style: TextStyle(
-                                        
-                                          fontSize: 20.0, 
-                                          color: Colors.brown
-                                          ),
-                                      decoration: InputDecoration(
-                                        
-                                          border: InputBorder.none,
-                                          hintText: '0,00',
-                                          hintStyle: TextStyle(
-                                              color: Colors.grey[400])),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 30),
-                          ),
-                          buildCardResult(
-                              "Real Brasileiro",
-                              "R\$",
-                              Colors.green[900],
-                              'BR.png',
-                              "Libras",
-                              "£",
-                              Colors.amber,
-                              'UK.png',
-                              realController,
-                              bitcController,),Padding(
-                            padding: EdgeInsets.only(top: 30),
-                          ),
-                          buildCardResult(
-                              "Dólar Americano",
-                              "US\$",
-                              Colors.red[700],
-                              'EUA.png',
-                              "Bitcoin",
-                              "BTC",
-                              Colors.amber,
-                              'BTC.png',
-                              dolarController,
-                              bitcController),
-                          Padding(
-                            padding: EdgeInsets.only(top: 30),
-                          ),
-                          buildCardResult(
-                              "Euro",
-                              "€",
-                              Colors.blue[900],
-                              'UE.png',
-                              "Real Brasileiro",
-                              "\$",
-                              Colors.lightBlue[600],
-                              'BR.png',
-                              euroController,
-                              realController),
+													buildCardResult(Colors.green[800],"Real Brasileiro",'BR',"R\$ ",realController, _realToAll),
+													buildCardResult(Colors.red[800],"Dolár Americano",'EUA',"US\$",dolarController, _dolarToAll),
+													buildCardResult(Colors.blue[900],"Euro",'UE'," € ",euroController, _euroToAll),
+													buildCardResult(Colors.orange,"Bitcoin",'BTC',"BTC",bitcController, _bitcToAll),
                         ],
                       ),
                     );
@@ -226,121 +179,91 @@ class _HomeState extends State<Home> {
   }
 }
 
-Widget buildCardResult(
-  String title1,
-  String prefix1,
-  Color color1,
-  String logo1,
-  String title2,
-  String prefix2,
-  Color color2,
-  String logo2,
-  TextEditingController c1,
-  TextEditingController c2
-) {
-  return Row(
-    children: <Widget>[
-      Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            color: color1,
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          margin: EdgeInsets.all(5.0),
-          height: 220.0,
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 7.0),
-                child: Text(
-                  "$title1",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                  ),
+Widget buildCardResult(Color color, String titulo, String img, String prefix, TextEditingController controller, Function functionConverter) {
+  return Container(
+    height: 140.0,
+		margin: EdgeInsets.only(top:30),
+    padding: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: [
+				BoxShadow(
+					color: Colors.grey,
+					blurRadius: 2.0
+				)
+			]
+		),
+    child: Row(
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Container(
+							width: 150.0,
+              child: Text(
+                titulo,
+								textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 18.0,
                 ),
               ),
-              Divider(),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('assets/images/$logo1'),
-                    )),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 23.0),
-                child: TextFormField(
-                  
-                  controller: c1,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                  decoration: InputDecoration(
-                    prefixStyle: TextStyle(fontSize: 30.0, color: Colors.white, fontWeight: FontWeight.bold),
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+            Divider(),
+            Container(
+              height: 50.0,
+              width: 50.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/$img.png'),
+                )
+							),
+            ),
+          ],
         ),
-      ),
-      Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            color: color2,
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          margin: EdgeInsets.all(5.0),
-          height: 220.0,
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 7.0),
-                child: Text(
-                  "$title2",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              Divider(),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('assets/images/$logo2'),
-                    )),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 23.0),
-                child: TextFormField(
-                  controller: c2,
-                  
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-            ],
-          ),
+
+        //divisor feito com container
+        Container(
+          height: 100.0,
+          width: 1.0,
+          color: Colors.grey,
+          margin: const EdgeInsets.only(left: 6.0, right: 14.0),
         ),
-      ),
-    ],
+
+				Expanded(
+					child: Container(
+						child: TextField(
+							controller: controller,
+							onChanged: functionConverter,
+							keyboardType: TextInputType.number,
+							style: TextStyle(
+								
+								color: Colors.white,
+								fontSize: 38.0
+							),
+							cursorColor: Colors.white,
+							
+							decoration: InputDecoration(
+						
+								prefix: Text(prefix),
+								prefixStyle: TextStyle(
+									color: Colors.white,
+									fontSize: 38.0
+								),
+								border: InputBorder.none,
+                hintText: "0,00",
+                hintStyle: TextStyle(
+									color: Colors.grey[400],
+									fontSize: 38.0
+								)
+							),
+						),
+					),
+				)
+      ],
+    ),
   );
 }
